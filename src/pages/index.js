dd63cf730aa5efad6e7b85d276e111639055d6ae
Kamil Rybiński <kamilryba19@gmail.com>
2020-02-10 22:50:04 +0100

    SimpleHero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import SimpleHero from '../components/SimpleHero';
 
 const Home = () => {
   return (
     <Layout>
-      <p>Index page</p>
+      <SimpleHero />
     </Layout>
   );
 };
