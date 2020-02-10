35395209d3fd34ac661a6c3e8e7c69dbe19e3246
Kamil Rybiński <kamilryba19@gmail.com>
2020-02-10 22:32:36 +0100

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
+import { FaAlignRight } from 'react-icons/fa';
+import styles from '../css/navbar.module.css';
+import links from '../constants/links';
+import socialIcons from '../constants/social-icons';
 
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  const [isOpen, setNav] = useState(false);
+
+  const toggleNav = () => setNav(isOpen => !isOpen);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <span className={styles.brandLogo}>TOURISM WEBSITE</span>
+          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+        <ul
+          className={
+            isOpen
+              ? `${styles.navLinks} ${styles.showNav}`
+              : `${styles.navLinks}`
+          }
+        >
+          {links.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={styles.navSocialLinks}>
+          {socialIcons.map((item, index) => {
+            return (
+              <a
+                key={index}
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.icon}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
