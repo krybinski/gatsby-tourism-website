b86b5b3e4f84843e4d2eb02e4116d8c60f52e651
Kamil Rybiński <kamilryba19@gmail.com>
2020-02-10 22:54:36 +0100

    Banner inside SimpleHero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import SimpleHero from '../components/SimpleHero';
+import Banner from '../components/Banner';
 
 const Home = () => {
   return (
     <Layout>
-      <SimpleHero />
+      <SimpleHero>
+        <Banner
+          title="Tourism website"
+          info="Welcome on one of the bigges tourism websites worldwide."
+        >
+          <Link to="/places" className="btn-white">
+            explore places
+          </Link>
+        </Banner>
+      </SimpleHero>
     </Layout>
   );
 };
